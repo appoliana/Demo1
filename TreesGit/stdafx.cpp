120603f3157e6108567d8a0ce655ebf44a68c7dc
polina picchio <polinapicchio@MacBook-Pro-polina.local>
2017-10-27 16:14:48 +0300

    First commit

---
+++ TreesGit/stdafx.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   stdafx.cpp
+ * Author: polinapicchio
+ * 
+ * Created on 27 октября 2017 г., 16:10
+ */
+
+#include "stdafx.h"
+
+stdafx::stdafx() {
+}
+
+stdafx::stdafx(const stdafx& orig) {
+}
+
+stdafx::~stdafx() {
+}
+
---
+++ TreesGit/stdafx.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   stdafx.h
+ * Author: polinapicchio
+ *
+ * Created on 27 октября 2017 г., 16:10
+ */
+
+#ifndef STDAFX_H
+#define	STDAFX_H
+
+class stdafx {
+public:
+    stdafx();
+    stdafx(const stdafx& orig);
+    virtual ~stdafx();
+private:
+
+};
+
+#endif	/* STDAFX_H */
+
